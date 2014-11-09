ce17f5f2fafe837052eb440cd9fa338272c2d09d
haswell <josiah.haswell@ca.com>
2014-11-09 12:03:26 -0700

    adding initial client structure

---
+++ client/Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        connect: {
+            server: {
+                options: {
+                    port: 8081,
+                    base: '.',
+                    keepalive:true
+                }
+            }
+        },
+
+        less : {
+            development : {
+
+            },
+            production : {
+                options : [ {
+                    paths: 'less'
+                }],
+                files : {
+                    'css/application.css' :
+                        'less/application.less'
+                }
+            }
+        },
+
+
+        watch : {
+            files :'less/*',
+            tasks: ['less']
+        }
+
+    });
+
+    grunt.loadNpmTasks("grunt-bowercopy");
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-compress");
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-contrib-less");
+};
