d1278d7d074b8d30f5488abbd57138f838438e77
Josiah Haswell <josiah.haswell@ca.com>
2015-02-07 17:14:40 -0700

    Fixing tests with dependencies

--- service/service-core/src/main/java/com/lab/ly/configuration/services/ServiceConfiguration.java
+++ service/service-core/src/main/java/com/lab/ly/configuration/services/ServiceConfiguration.java
@@ -1,6 +1,7 @@
 package com.lab.ly.configuration.services;
 
 import com.lab.ly.configurations.model.ModelConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Import({
     ModelConfiguration.class
 })
+@ComponentScan(basePackages = "com.lab.ly.service")
 @Configuration
 @EnableTransactionManagement
 public class ServiceConfiguration {
